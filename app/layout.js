0a4267f0d05db5c7cf36129acd653ac44e952390
conan-kim <conan.kim@hyperhire.in>
2024-07-24 22:49:13 +0900

    test3

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="whitespace-pre relative h-[100vh] break-keep">
+      <body className="whitespace-pre relative h-svh break-keep">
         <LoadingProvider>
           <PlatformProvider>
             <div className="max-w-screen-md m-auto">{children}</div>
