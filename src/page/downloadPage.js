c1b84437a534930733b8d713e66b2e12902ce00a
conan-kim <conan.kim@hyperhire.in>
2024-10-21 18:38:37 +0900

    app download on download page

--- src/page/downloadPage.js
+++ src/page/downloadPage.js
@@ -7,9 +7,11 @@ import LogoSvg from "@/public/logo.svg";
 import DownloadSvg from "@/public/download.svg";
 import GoogleDownloadSvg from "@/public/download-google.svg";
 import AppleDownloadSvg from "@/public/download-appstore.svg";
+import { usePlatform } from "../../context/platformContext";
 
 const DownloadPage = () => {
   const [platform, setPlatform] = useState("desktop");
+  const { getDownloadLink } = usePlatform();
 
   useEffect(() => {
     const userAgent = navigator.userAgent || navigator.vendor || window.opera;
@@ -28,7 +30,7 @@ const DownloadPage = () => {
       <div className="flex flex-col flex-1 w-full">
         {platform === "android" || platform === "ios" ? (
           <a
-            href=""
+            href={getDownloadLink()}
             target="_blank"
             className="flex flex-row cursor-pointer justify-center items-center mx-[20px] py-[10px] rounded-[16px] bg-gradient-to-r from-[#00FF94] to-[#00E5CA]"
           >
@@ -37,10 +39,18 @@ const DownloadPage = () => {
           </a>
         ) : (
           <div className="flex flex-row gap-4 items-center justify-center">
-            <a href="" target="_blank" className="cursor-pointer">
+            <a
+              href="https://apps.apple.com/app/%EB%AF%B8%EB%AF%B8%EC%BD%98-mimecon/id6504627348"
+              target="_blank"
+              className="cursor-pointer"
+            >
               <AppleDownloadSvg />
             </a>
-            <a href="" target="_blank" className="cursor-pointer">
+            <a
+              href="https://play.google.com/store/apps/details?id=com.ploonet.mimecon"
+              target="_blank"
+              className="cursor-pointer"
+            >
               <GoogleDownloadSvg />
             </a>
           </div>
